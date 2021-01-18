7057bb286255a9a38db78824505f6ea5be9da147
Taha Al Khashmany <56194280+Taha-cmd@users.noreply.github.com>
2021-01-18 16:35:47 +0100

    Update view_user_session.sql

--- database/_views/view_user_session.sql
+++ database/_views/view_user_session.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE VIEW "view_user_session" AS
 	SELECT "first_name","last_name","session"."id","email"
 	FROM "user"
-	JOIN "session" on "user"."id" = "session"."user_id"
+	JOIN "session" on "user"."id" = "session"."user_id";
