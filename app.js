f1b7ea5001de272d9196beee4a8f79b74e625629
Kjubie Bruckner <abru1322@gmail.com>
2021-01-19 22:33:26 +0100

    Login und Register wörkt

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const path = require("path");
 const fileUpload = require("express-fileupload");
 const { v4: uuidv4 } = require("uuid");
 const oracledb = require('oracledb');
+const session = require('express-session');
+const password = require('password-hash-and-salt');
 
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+app.use(session({secret: 'notASecret',saveUninitialized: true,resave: true}));
 app.use(express.static(path.join(__dirname, "public")));
 
 // for parsing application/json
@@ -26,61 +29,83 @@ app.use(
 	})
 );
 
-let loggedIn = true;
+var sess;
+var loggedIn = false;
 
 app.get("/", (req, res) =>
 	!loggedIn
 		? res.render("index", { data: { menu: menu } })
 		: res.render("homepage", {
 				data: {
-					username: "taha",
+					username: sess.username,
 					profile_pic: "images/cat.png",
 					menu: menu,
 					loggedIn: loggedIn,
 				},
 		  })
 );
+
 app.get("/browse", (req, res) =>
 	res.render("homepage", { data: { loggedIn: false, menu: menuNotLoggedIn } })
 );
-app.get("/login", (req, res) => res.render("login", { data: { menu: menu } }));
+
+app.get("/login", (req, res) => 
+	res.render("login", { data: { menu: menu } })
+);
+
 app.get("/register", (req, res) =>
 	res.render("register", { data: { menu: menu } })
 );
 
 app.post('/login', async function (req, res) {
 	let connection;
+	var mypw = "dbsw20";
 	var pwd = null;
 	
 	try{
 		connection = await oracledb.getConnection( {
-			user          : "hr",
+			user          : "w20bif3_if20b185",
 			password      : mypw,
-			connectString : "localhost/XEPDB1"
+			connectString : "infdb.technikum-wien.at:1521/o10"
 		});
 
-		var result = await connection.execute("select id, password_hash from user where username = :un", [req.body.username]);
-		pwd = result.rows[0]["password_hash"];
+		await connection.execute("select \"id\", \"password_hash\" from \"user\" where \"username\" = :un", [req.body.username], function(err, result){
+			if (err) {
+				console.log('Result: ', err);
+				res.sendStatus(400);
+				connection.close();
+			} else {
+				console.log('Result: ', result);
+				pwd = result.rows[0]["password_hash"];
 		
-		password(req.body.password).verifyAgainst(pwd, function(error, verified) {
-            if(error)
-                console.log(error);
-            if(!verified) {
-                res.sendStatus(401);
-            } else {
-				var uid = result.rows[0]["id"];
+				password(req.body.password).verifyAgainst(pwd, async function(error, verified) {
+					if(error)
+						console.log(error);
+					if(!verified) {
+						res.sendStatus(401);
+					} else {
+						var uid = result.rows[0]["id"];
+						
+						//await connection.execute("insert into \"session\" values(0, CURRENT_TIMESTAMP, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 1 DAY), :id)", [uid]);
+						
+						sess = req.session;
+						sess.username = req.body.username;
+						loggedIn = true;
+						
+						res.redirect('http://127.0.0.1:8080/');
+					}
+				});
 				
-				await connection.execute("insert into session values(0, CURRENT_TIMESTAMP, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 1 DAY), :id)", [uid]);
-                res.sendStatus(200);
-            }
-        });
+				connection.close();
+			}
+		});
 		
 	} catch(err){
 		console.error(err);
 	} finally {
 		if (connection) {
 			try {
-				await connection.close();
+				//await connection.close();
 			} catch (err) {
 				console.error(err);
 			}
@@ -89,45 +114,52 @@ app.post('/login', async function (req, res) {
 });
 
 app.post("/register", async function (req, res) {
-	let connection;
-	var pwd = null;
+	var connection;
+	var mypw = "dbsw20";
 	
 	try{
-		connection = await oracledb.getConnection( {
-			user          : "hr",
+		connection = await oracledb.getConnection({
+			user          : "w20bif3_if20b185",
 			password      : mypw,
-			connectString : "localhost/XEPDB1"
+			connectString : "infdb.technikum-wien.at:1521/o10"
 		});
-		
+			
 		res.set('Content-Type', 'text/html');
-		
-		password(req.body.psw).hash(function(error, hash) {
+	
+		password(req.body.psw).hash(async function(error, hash) {
 			if(error)
 				throw new Error('Something went wrong!');
 
-			pwd = hash;
+			var pwd = hash;
 			
-			await connection.execute("insert into user" + 
-									"values" + 
-									"(0, :fn, :ln, :un, :pw, :em), :id)", 
-									[req.body.first_name, req.body.last_name, req.body.username, pwd, req.body.email], 
+			var st = "insert into \"user\" " + 
+					"values" + 
+					"(0, '" + req.body.first_name + "', '" + req.body.last_name + "', '" + req.body.username + "', '" + pwd + "', '" + req.body.email + "', 0, 0, 0, 0)"
+			
+			console.log('Insert: ', st);
+			
+			await connection.execute(st, 
+									[], {autoCommit: true},
 									function(err, result){
 										if (err) {
+											console.log('Result: ', err);
 											res.sendStatus(400);
+											connection.close();
 										} else{
-											res.sendStatus(200);
+											console.log('Result: ', result);
+											res.redirect('http://127.0.0.1:8080/');
+											connection.close();
 										}
-									});    
+									});
+
 		});
-		
-		res.redirect('http://127.0.0.1/home);
 		
 	} catch(err){
 		console.error(err);
 	} finally {
 		if (connection) {
 			try {
-				await connection.close();
+				//await connection.close();
 			} catch (err) {
 				console.error(err);
 			}
