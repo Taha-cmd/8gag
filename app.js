3eb76636d6c051231649dab1eece2578a762fc0d
Kjubie Bruckner <abru1322@gmail.com>
2021-01-20 19:12:27 +0100

    Fixed Stuff

--- app.js
+++ app.js
@@ -74,43 +74,31 @@ app.get("/browse", (req, res) =>
 
 app.get("/boards", async function (req, res) {
 	let connection;
-
-	try {
-		connection = await oracledb.getConnection({
-			user: config.database.user,
-			password: config.database.password,
-			connectString: config.database.connectString,
+	var mypw = "dbsw20";
+	var pwd = null;
+	
+	try{
+		connection = await oracledb.getConnection( {
+			user          : "w20bif3_if20b185",
+			password      : mypw,
+			connectString : "infdb.technikum-wien.at:1521/o10"
 		});
 
-		await connection.execute(
-			'select * from "board"',
-			[],
-			function (err, result) {
-				if (err) {
-					console.log("Result: ", err);
-					res.sendStatus(400);
-					connection.close();
-				} else {
-					if (loggedIn)
-						res.render("boards", {
-							data: {
-								loggedIn: true,
-								menu: menuLoggedIn,
-								uid: sess.uid,
-								boards: result,
-								username: sess.username,
-								subs: sess.subs,
-							},
-						});
-					else
-						res.render("boards", {
-							data: { loggedIn: false, menu: menuNotLoggedIn, boards: result },
-						});
-					connection.close();
-				}
+		await connection.execute("select * from \"board\"", [], function(err, result){
+			if (err) {
+				console.log('Result: ', err);
+				res.sendStatus(400);
+				connection.close();
+			} else {
+				if(loggedIn)
+					res.render("boards", { data: { loggedIn: true, menu: menuLoggedIn, uid: sess.uid, boards: result, username: sess.username, subs: sess.subs} })
+				else
+					res.render("boards", { data: { loggedIn: false, menu: menuNotLoggedIn, boards: result} })
+				connection.close();
 			}
-		);
-	} catch (err) {
+		});
+		
+	} catch(err){
 		console.error(err);
 	} finally {
 		if (connection) {
@@ -120,62 +108,42 @@ app.get("/boards", async function (req, res) {
 				console.error(err);
 			}
 		}
-	}
+	}	
 });
 
 app.get("/board", async function (req, res) {
-	const queryObject = url.parse(req.url, true).query;
+	const queryObject = url.parse(req.url,true).query;
 	console.log("Request: ", queryObject["boardName"]);
-
+	
 	let connection;
 	var mypw = "dbsw20";
 	var pwd = null;
-
-	try {
-		connection = await oracledb.getConnection({
-			user: config.database.user,
-			password: config.database.password,
-			connectString: config.database.connectString,
+	
+	try{
+		connection = await oracledb.getConnection( {
+			user          : "w20bif3_if20b185",
+			password      : mypw,
+			connectString : "infdb.technikum-wien.at:1521/o10"
 		});
 
-		await connection.execute(
-			"select * from" +
-				'(select "user"."id", "board_post"."board_id", "content", "username", "board_post"."id" as BID from "board_post" inner join "user" on "user"."id" = "board_post"."user_id")' +
-				'where "board_id" = :id order by BID',
-			[queryObject["board"]],
-			function (err, result) {
-				if (err) {
-					console.log("Result: ", err);
-					res.sendStatus(400);
-					connection.close();
-				} else {
-					if (loggedIn)
-						res.render("board", {
-							data: {
-								loggedIn: true,
-								menu: menuLoggedIn,
-								uid: sess.uid,
-								board: result,
-								boardName: queryObject["boardName"],
-								boardID: queryObject["board"],
-								username: sess.username,
-							},
-						});
-					else
-						res.render("board", {
-							data: {
-								loggedIn: false,
-								menu: menuNotLoggedIn,
-								board: result,
-								boardName: queryObject["boardName"],
-								boardID: queryObject["board"],
-							},
-						});
-					connection.close();
-				}
+		await connection.execute("select * from" + 
+								"(select \"user\".\"id\", \"board_post\".\"board_id\", \"content\", \"username\", \"board_post\".\"id\" as BID from \"board_post\" inner join \"user\" on \"user\".\"id\" = \"board_post\".\"user_id\")" + 
+								"where \"board_id\" = :id order by BID",
+								[queryObject["board"]], function(err, result){
+			if (err) {
+				console.log('Result: ', err);
+				res.sendStatus(400);
+				connection.close();
+			} else {
+				if(loggedIn)
+					res.render("board", { data: { loggedIn: true, menu: menuLoggedIn, uid: sess.uid, board: result, boardName: queryObject["boardName"], boardID: queryObject["board"], username: sess.username} })
+				else
+					res.render("board", { data: { loggedIn: false, menu: menuNotLoggedIn, board: result, boardName: queryObject["boardName"], boardID: queryObject["board"]} })
+				connection.close();
 			}
-		);
-	} catch (err) {
+		});
+		
+	} catch(err){
 		console.error(err);
 	} finally {
 		if (connection) {
@@ -267,78 +235,72 @@ app.get("/register", (req, res) => {
 	res.render("register", { data: { menu: menu } });
 });
 
-app.post("/login", async function (req, res) {
+app.post('/login', async function (req, res) {
 	let connection;
+	var mypw = "dbsw20";
 	var pwd = null;
 	var res_user;
-
-	try {
-		connection = await oracledb.getConnection({
-			user: config.database.user,
-			password: config.database.password,
-			connectString: config.database.connectString,
+	
+	try{
+		connection = await oracledb.getConnection( {
+			user          : "w20bif3_if20b185",
+			password      : mypw,
+			connectString : "infdb.technikum-wien.at:1521/o10"
 		});
 
-		await connection.execute(
-			'select "id", "password_hash" from "user" where "username" = :un',
-			[req.body.username],
-			function (err, result) {
-				if (err) {
-					console.log("Result: ", err);
-					res.sendStatus(400);
+		await connection.execute("select \"id\", \"password_hash\" from \"user\" where \"username\" = :un", [req.body.username], function(err, result){
+			if (err) {
+				console.log('Result: ', err);
+				res.sendStatus(400);
+				connection.close();
+			} else {
+				pwd = result.rows[0]["password_hash"];
+		
+				password(req.body.password).verifyAgainst(pwd, async function(error, verified) {
+					if(error)
+						console.log(error);
+					if(!verified) {
+						res.sendStatus(401);
+					} else {
+						res_user = result; 
+						
+						sess = req.session;
+						sess.uid = res_user.rows[0]["id"];
+						sess.username = req.body.username;
+						loggedIn = true;
+					}
+					
 					connection.close();
-				} else {
-					pwd = result.rows[0]["password_hash"];
-
-					password(req.body.password).verifyAgainst(
-						pwd,
-						async function (error, verified) {
-							if (error) console.log(error);
-							if (!verified) {
-								res.sendStatus(401);
-							} else {
-								res_user = result;
-
-								sess = req.session;
-								sess.uid = res_user.rows[0]["id"];
-								sess.username = req.body.username;
-								loggedIn = true;
-							}
-
-							connection.close();
-						}
-					);
-				}
+				});
 			}
-		);
-
-		connection2 = await oracledb.getConnection({
-			user: config.database.user,
-			password: config.database.password,
-			connectString: config.database.connectString,
 		});
-
-		if (loggedIn) {
-			await connection2.execute(
-				'select "board_id" from "subscription" where "user_id" = :id',
-				[res_user.rows[0]["id"]],
-				function (err, result) {
-					if (err) console.log(err);
-					else {
-						console.log(result.rows);
-						sess.subs = new Array();
-						for (var i = 0; i < result.rows.length; i++) {
-							sess.subs[i] = result.rows[i]["board_id"];
-						}
-
-						res.redirect("http://127.0.0.1:8080/");
+		
+		connection2 = await oracledb.getConnection( {
+			user          : "w20bif3_if20b185",
+			password      : mypw,
+			connectString : "infdb.technikum-wien.at:1521/o10"
+		});
+		
+		
+		if(loggedIn){
+			await connection2.execute("select \"board_id\" from \"subscription\" where \"user_id\" = :id", [res_user.rows[0]["id"]], function(err, result){
+				if(err)
+					console.log(err);
+				else {
+					console.log(result.rows);
+					sess.subs = new Array();
+					for(var i = 0; i < result.rows.length; i++) {
+						sess.subs[i] = result.rows[i]["board_id"];
 					}
 
-					connection2.close();
+					res.redirect('http://127.0.0.1:8080/');
 				}
-			);
+				
+				connection2.close();
+			});
 		}
-	} catch (err) {
+		
+	} catch(err){
 		console.error(err);
 	} finally {
 		if (connection) {
@@ -353,53 +315,43 @@ app.post("/login", async function (req, res) {
 
 app.post("/register", async function (req, res) {
 	var connection;
-
-	try {
+	var mypw = "dbsw20";
+	
+	try{
 		connection = await oracledb.getConnection({
-			user: config.database.user,
-			password: config.database.password,
-			connectString: config.database.connectString,
+			user          : "w20bif3_if20b185",
+			password      : mypw,
+			connectString : "infdb.technikum-wien.at:1521/o10"
 		});
-
-		res.set("Content-Type", "text/html");
-
-		password(req.body.psw).hash(async function (error, hash) {
-			if (error) throw new Error("Something went wrong!");
+			
+		res.set('Content-Type', 'text/html');
+	
+		password(req.body.psw).hash(async function(error, hash) {
+			if(error)
+				throw new Error('Something went wrong!');
 
 			var pwd = hash;
+			
+			var st = "insert into \"user\" " + 
+					"(\"first_name\", \"last_name\", \"username\", \"password_hash\", \"email\") values" + 
+					"('" + req.body.first_name + "', '" + req.body.last_name + "', '" + req.body.username + "', '" + pwd + "', '" + req.body.email + "')";
 
-			var st =
-				'insert into "user" ' +
-				'("first_name", "last_name", "username", "password_hash", "email") values' +
-				"('" +
-				req.body.first_name +
-				"', '" +
-				req.body.last_name +
-				"', '" +
-				req.body.username +
-				"', '" +
-				pwd +
-				"', '" +
-				req.body.email +
-				"')";
+			await connection.execute(st, 
+									[], {autoCommit: true},
+									function(err, result){
+										if (err) {
+											console.log('Result: ', err);
+											res.sendStatus(400);
+											connection.close();
+										} else{
+											res.redirect('http://127.0.0.1:8080/');
+											connection.close();
+										}
+									});
 
-			await connection.execute(
-				st,
-				[],
-				{ autoCommit: true },
-				function (err, result) {
-					if (err) {
-						console.log("Result: ", err);
-						res.sendStatus(400);
-						connection.close();
-					} else {
-						res.redirect("http://127.0.0.1:8080/");
-						connection.close();
-					}
-				}
-			);
 		});
-	} catch (err) {
+		
+	} catch(err){
 		console.error(err);
 	} finally {
 		if (connection) {
@@ -478,44 +430,30 @@ app.post("/uploadBoardPost", async function (req, res) {
 
 	let connection;
 	var mypw = "dbsw20";
-
-	try {
-		connection = await oracledb.getConnection({
-			user: config.database.user,
-			password: config.database.password,
-			connectString: config.database.connectString,
+	
+	try{
+		connection = await oracledb.getConnection( {
+			user          : "w20bif3_if20b185",
+			password      : mypw,
+			connectString : "infdb.technikum-wien.at:1521/o10"
 		});
-
-		var st =
-			'insert into "board_post" ("content", "user_id", "board_id") values (\'' +
-			req.body.post_content +
-			"', " +
-			sess.uid +
-			", " +
-			req.body.board_id +
-			")";
-
-		await connection.execute(
-			st,
-			[],
-			{ autoCommit: true },
-			function (err, result) {
-				if (err) {
-					console.log("Result: ", err);
-					res.sendStatus(400);
-					connection.close();
-				} else {
-					res.redirect(
-						"http://127.0.0.1:8080/board?board=" +
-							req.body.board_id +
-							"&boardName=" +
-							req.body.board_name
-					);
-					connection.close();
-				}
+	
+		var st = "insert into \"board_post\" (\"content\", \"user_id\", \"board_id\") values ('" + req.body.post_content + "', " + sess.uid + ", " + req.body.board_id + ")";
+		
+		await connection.execute(st,
+								[], {autoCommit: true}, 
+								function(err, result){
+			if (err) {
+				console.log('Result: ', err);
+				res.sendStatus(400);
+				connection.close();
+			} else {
+				res.redirect('http://127.0.0.1:8080/board?board=' + req.body.board_id + '&boardName=' + req.body.board_name);
+				connection.close();
 			}
-		);
-	} catch (err) {
+		});
+		
+	} catch(err){
 		console.error(err);
 	} finally {
 		if (connection) {
@@ -533,38 +471,33 @@ app.post("/sub", async function (req, res) {
 
 	let connection;
 	var mypw = "dbsw20";
-
-	try {
-		connection = await oracledb.getConnection({
-			user: config.database.user,
-			password: config.database.password,
-			connectString: config.database.connectString,
+	
+	try{
+		connection = await oracledb.getConnection( {
+			user          : "w20bif3_if20b185",
+			password      : mypw,
+			connectString : "infdb.technikum-wien.at:1521/o10"
 		});
-
-		var st =
-			'insert into "subscription" ("user_id", "board_id") values (' +
-			sess.uid +
-			", " +
-			req.body.board_id +
-			")";
-
-		await connection.execute(
-			st,
-			[],
-			{ autoCommit: true },
-			function (err, result) {
-				if (err) {
-					console.log("Result: ", err);
-					res.sendStatus(400);
-					connection.close();
-				} else {
-					sess.subs[sess.subs.length] = Number(req.body.board_id);
-					res.redirect("http://127.0.0.1:8080/boards");
-					connection.close();
-				}
+	
+		var st = "insert into \"subscription\" (\"user_id\", \"board_id\") values (" + sess.uid + ", " + req.body.board_id + ")";
+		
+		
+		
+		await connection.execute(st,
+								[], {autoCommit: true}, 
+								function(err, result){
+			if (err) {
+				console.log('Result: ', err);
+				res.sendStatus(400);
+				connection.close();
+			} else {
+				sess.subs[sess.subs.length] = Number(req.body.board_id);
+				res.redirect('http://127.0.0.1:8080/boards');
+				connection.close();
 			}
-		);
-	} catch (err) {
+		});
+		
+	} catch(err){
 		console.error(err);
 	} finally {
 		if (connection) {
@@ -579,39 +512,37 @@ app.post("/sub", async function (req, res) {
 
 app.post("/unSub", async function (req, res) {
 	console.log("Body: ", req.body);
-	console.log(sess);
 
 	let connection;
-
-	try {
-		connection = await oracledb.getConnection({
-			user: config.database.user,
-			password: config.database.password,
-			connectString: config.database.connectString,
+	var mypw = "dbsw20";
+	
+	try{
+		connection = await oracledb.getConnection( {
+			user          : "w20bif3_if20b185",
+			password      : mypw,
+			connectString : "infdb.technikum-wien.at:1521/o10"
 		});
-
-		//var st = `delete from "subscription" where "user_id" = ${sess.uid} and "board_id" = ${req.body.board_id}`;
+	
 		var st = "delete from \"subscription\" where \"user_id\" = " + sess.uid + " and \"board_id\" = " + req.body.board_id;
-
-		await connection.execute(
-			st,
-			[sess.uid, req.body.board_id],
-			{ autoCommit: true },
-			function (err, result) {
-				if (err) {
-					console.log("Result: ", err);
-					res.sendStatus(400);
-					connection.close();
-				} else {
-					for (var i = 0; i < sess.subs.length; i++)
-						if (sess.subs[i] == Number(req.body.board_id)) sess.subs[i] = -1;
-
-					res.redirect("http://127.0.0.1:8080/boards");
-					connection.close();
-				}
+		
+		await connection.execute(st,
+								[], {autoCommit: true}, 
+								function(err, result){
+			if (err) {
+				console.log('Result: ', err);
+				res.sendStatus(400);
+				connection.close();
+			} else {
+				for(var i = 0; i < sess.subs.length; i++)
+					if(sess.subs[i] == Number(req.body.board_id))
+						sess.subs[i] = -1;
+					
+				res.redirect('http://127.0.0.1:8080/boards');
+				connection.close();
 			}
-		);
-	} catch (err) {
+		});
+		
+	} catch(err){
 		console.error(err);
 	} finally {
 		if (connection) {
@@ -626,38 +557,30 @@ app.post("/unSub", async function (req, res) {
 
 app.get("/notifications", async function (req, res) {
 	let connection;
-
-	try {
-		connection = await oracledb.getConnection({
-			user: config.database.user,
-			password: config.database.password,
-			connectString: config.database.connectString,
+	var mypw = "dbsw20";
+	var pwd = null;
+	
+	try{
+		connection = await oracledb.getConnection( {
+			user          : "w20bif3_if20b185",
+			password      : mypw,
+			connectString : "infdb.technikum-wien.at:1521/o10"
 		});
 
-		await connection.execute(
-			'select * from "notification" where "user_id" = :id',
-			[sess.uid],
-			function (err, result) {
-				if (err) {
-					console.log("Result: ", err);
-					res.sendStatus(400);
-					connection.close();
-				} else {
-					console.log("Result: ", result);
-					if (loggedIn)
-						res.render("notifications", {
-							data: {
-								loggedIn: true,
-								menu: menuLoggedIn,
-								uid: sess.uid,
-								notifications: result,
-							},
-						});
-					connection.close();
-				}
+		await connection.execute("select * from \"notification\" where \"user_id\" = :id", [sess.uid], function(err, result){
+			if (err) {
+				console.log('Result: ', err);
+				res.sendStatus(400);
+				connection.close();
+			} else {
+				console.log('Result: ', result);
+				if(loggedIn)
+					res.render("notifications", { data: { loggedIn: true, menu: menuLoggedIn, uid: sess.uid, notifications: result}})
+				connection.close();
 			}
-		);
-	} catch (err) {
+		});
+		
+	} catch(err){
 		console.error(err);
 	} finally {
 		if (connection) {
@@ -667,39 +590,38 @@ app.get("/notifications", async function (req, res) {
 				console.error(err);
 			}
 		}
-	}
+	}	
 });
 
 app.post("/delNote", async function (req, res) {
 	console.log("Body: ", req.body);
 
 	let connection;
-
-	try {
-		connection = await oracledb.getConnection({
-			user: config.database.user,
-			password: config.database.password,
-			connectString: config.database.connectString,
+	var mypw = "dbsw20";
+	
+	try{
+		connection = await oracledb.getConnection( {
+			user          : "w20bif3_if20b185",
+			password      : mypw,
+			connectString : "infdb.technikum-wien.at:1521/o10"
 		});
-
-		var st = 'delete from "notification" where "user_id" = ' + sess.uid;
-
-		await connection.execute(
-			st,
-			[],
-			{ autoCommit: true },
-			function (err, result) {
-				if (err) {
-					console.log("Result: ", err);
-					res.sendStatus(400);
-					connection.close();
-				} else {
-					res.redirect("http://127.0.0.1:8080/notifications");
-					connection.close();
-				}
+	
+		var st = "exec deleteNotificationByUserID(" + sess.uid + ")";
+		
+		await connection.execute(st,
+								[], {autoCommit: true}, 
+								function(err, result){
+			if (err) {
+				console.log('Result: ', err);
+				res.sendStatus(400);
+				connection.close();
+			} else {
+				res.redirect('http://127.0.0.1:8080/notifications');
+				connection.close();
 			}
-		);
-	} catch (err) {
+		});
+		
+	} catch(err){
 		console.error(err);
 	} finally {
 		if (connection) {
@@ -758,7 +680,7 @@ app.post("/likes/:id", async (req, res) => {
 		});
 
 		await connection.execute(
-			`insert into "post_likes"("user_id", "post_id") 
+			`insert into "post_likes"("user_id", "post_id")
 			values((select "id" from "user" where "username" = :username), :id)`,
 			[sess.username, req.params.id],
 			{ autoCommit: true },
