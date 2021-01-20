a7650c8c248b5d93243aa0e004b2de6e96379700
Taha Al Khashmany <taharaed98@gmail.com>
2021-01-20 15:07:26 +0100

    added posts

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-const path = require("path");
+const { join } = require("path");
+const { readFileSync } = require("fs");
 const fileUpload = require("express-fileupload");
 const { v4: uuidv4 } = require("uuid");
-const oracledb = require('oracledb');
-const session = require('express-session');
-const password = require('password-hash-and-salt');
+const oracledb = require("oracledb");
+const session = require("express-session");
+const password = require("password-hash-and-salt");
 
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-app.use(session({secret: 'notASecret',saveUninitialized: true,resave: true}));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(
+	session({ secret: "notASecret", saveUninitialized: true, resave: true })
+);
+app.use(express.static(join(__dirname, "public")));
 
 // for parsing application/json
 app.use(express.json());
@@ -29,78 +32,145 @@ app.use(
 	})
 );
 
+const config = JSON.parse(readFileSync("config.json"));
 var sess;
 var loggedIn = false;
 
-app.get("/", (req, res) =>
-	!loggedIn
-		? res.render("index", { data: { menu: menu } })
-		: res.render("homepage", {
-				data: {
-					username: sess.username,
-					profile_pic: "images/cat.png",
-					menu: menu,
-					loggedIn: loggedIn,
-				},
-		  })
-);
+const menuLoggedIn = [
+	{ name: "profile", href: "/profile" },
+	{ name: "homepage", href: "/" },
+];
 
-app.get("/browse", (req, res) =>
-	res.render("homepage", { data: { loggedIn: false, menu: menuNotLoggedIn } })
-);
+const menuNotLoggedIn = [
+	{ name: "homepage", href: "/" },
+	{ name: "login", href: "/login" },
+	{ name: "register", href: "/register" },
+];
 
-app.get("/login", (req, res) => 
-	res.render("login", { data: { menu: menu } })
-);
+app.get("/", async (req, res) => {
+	const menu = loggedIn ? menuLoggedIn : menuNotLoggedIn;
 
-app.get("/register", (req, res) =>
-	res.render("register", { data: { menu: menu } })
-);
+	const posts = [];
 
-app.post('/login', async function (req, res) {
-	let connection;
-	var mypw = "dbsw20";
-	var pwd = null;
-	
-	try{
-		connection = await oracledb.getConnection( {
-			user          : "w20bif3_if20b185",
-			password      : mypw,
-			connectString : "infdb.technikum-wien.at:1521/o10"
+	try {
+		connection = await oracledb.getConnection({
+			user: config.database.user,
+			password: config.database.password,
+			connectString: config.database.connectString,
 		});
 
-		await connection.execute("select \"id\", \"password_hash\" from \"user\" where \"username\" = :un", [req.body.username], function(err, result){
-			if (err) {
-				console.log('Result: ', err);
-				res.sendStatus(400);
-				connection.close();
-			} else {
-				console.log('Result: ', result);
-				pwd = result.rows[0]["password_hash"];
-		
-				password(req.body.password).verifyAgainst(pwd, async function(error, verified) {
-					if(error)
-						console.log(error);
-					if(!verified) {
-						res.sendStatus(401);
-					} else {
-						var uid = result.rows[0]["id"];
-						
-						//await connection.execute("insert into \"session\" values(0, CURRENT_TIMESTAMP, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 1 DAY), :id)", [uid]);
-						
-						sess = req.session;
-						sess.username = req.body.username;
-						loggedIn = true;
-						
-						res.redirect('http://127.0.0.1:8080/');
-					}
-				});
-				
-				connection.close();
+		await connection.execute(
+			`select * from "post" join "user" on "post"."user_id" = "user"."id"
+			join "post_tag" on "post"."id" = "post_tag"."post_id" join "tag" on "tag"."id" = "post_tag"."tag_id"
+			left join "post_image" on "post"."id" = "post_image"."post_id" 
+			left join "image" on "image"."id" = "post_image"."image_id"`,
+			(err, result) => {
+				if (err) {
+					console.log("Result: ", err);
+					connection.close();
+				} else {
+					console.log(result);
+					result.rows.forEach((row) => {
+						posts.push({
+							id: row["id"],
+							title: row["title"],
+							content: row["content"],
+							username: row["username"],
+							tag: row["name"],
+							image: row["path"] === null ? null : join("images", row["path"]),
+						});
+					});
+					!loggedIn
+						? res.render("index", { data: { menu: menu } })
+						: res.render("homepage", {
+								data: {
+									username: sess.username,
+									profile_pic: "images/cat.png",
+									menu: menu,
+									loggedIn: loggedIn,
+									posts: posts,
+								},
+						  });
+				}
 			}
+		);
+	} catch (err) {
+		console.error(err);
+	} finally {
+		if (connection) {
+			try {
+				//await connection.close();
+			} catch (err) {
+				console.error(err);
+			}
+		}
+		//posts.forEach((post) => (post["images"] = getImages(post.id)));
+	}
+});
+
+app.get("/browse", (req, res) => {
+	const menu = loggedIn ? menuLoggedIn : menuNotLoggedIn;
+	res.render("homepage", { data: { loggedIn: false, menu: menuNotLoggedIn } });
+});
+
+app.get("/login", (req, res) => {
+	const menu = loggedIn ? menuLoggedIn : menuNotLoggedIn;
+	res.render("login", { data: { menu: menu } });
+});
+
+app.get("/register", (req, res) => {
+	const menu = loggedIn ? menuLoggedIn : menuNotLoggedIn;
+	res.render("register", { data: { menu: menu } });
+});
+
+app.post("/login", async function (req, res) {
+	let connection;
+	var pwd = null;
+
+	try {
+		connection = await oracledb.getConnection({
+			user: config.database.user,
+			password: config.database.password,
+			connectString: config.database.connectString,
 		});
-		
-	} catch(err){
+
+		await connection.execute(
+			'select "id", "password_hash" from "user" where "username" = :un',
+			[req.body.username],
+			function (err, result) {
+				if (err) {
+					console.log("Result: ", err);
+					res.sendStatus(400);
+					connection.close();
+				} else {
+					//console.log("Result: ", result);
+					pwd = result.rows[0]["password_hash"];
+
+					password(req.body.password).verifyAgainst(
+						pwd,
+						async function (error, verified) {
+							if (error) console.log(error);
+							if (!verified) {
+								res.sendStatus(401);
+							} else {
+								var uid = result.rows[0]["id"];
+
+								//await connection.execute("insert into \"session\" values(0, CURRENT_TIMESTAMP, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 1 DAY), :id)", [uid]);
+
+								sess = req.session;
+								sess.username = req.body.username;
+								loggedIn = true;
+
+								res.redirect("http://127.0.0.1:8080/");
+							}
+						}
+					);
+
+					connection.close();
+				}
+			}
+		);
+	} catch (err) {
 		console.error(err);
 	} finally {
 		if (connection) {
@@ -115,46 +185,56 @@ app.post('/login', async function (req, res) {
 
 app.post("/register", async function (req, res) {
 	var connection;
-	var mypw = "dbsw20";
-	
-	try{
+
+	try {
 		connection = await oracledb.getConnection({
-			user          : "w20bif3_if20b185",
-			password      : mypw,
-			connectString : "infdb.technikum-wien.at:1521/o10"
+			user: config.database.user,
+			password: config.database.password,
+			connectString: config.database.connectString,
 		});
-			
-		res.set('Content-Type', 'text/html');
-	
-		password(req.body.psw).hash(async function(error, hash) {
-			if(error)
-				throw new Error('Something went wrong!');
+
+		res.set("Content-Type", "text/html");
+
+		password(req.body.psw).hash(async function (error, hash) {
+			if (error) throw new Error("Something went wrong!");
 
 			var pwd = hash;
-			
-			var st = "insert into \"user\" " + 
-					"values" + 
-					"(0, '" + req.body.first_name + "', '" + req.body.last_name + "', '" + req.body.username + "', '" + pwd + "', '" + req.body.email + "', 0, 0, 0, 0)"
-			
-			console.log('Insert: ', st);
-			
-			await connection.execute(st, 
-									[], {autoCommit: true},
-									function(err, result){
-										if (err) {
-											console.log('Result: ', err);
-											res.sendStatus(400);
-											connection.close();
-										} else{
-											console.log('Result: ', result);
-											res.redirect('http://127.0.0.1:8080/');
-											connection.close();
-										}
-									});
 
+			var st =
+				'insert into "user" ' +
+				"values" +
+				"(null, '" +
+				req.body.first_name +
+				"', '" +
+				req.body.last_name +
+				"', '" +
+				req.body.username +
+				"', '" +
+				pwd +
+				"', '" +
+				req.body.email +
+				"', null, null, null, null)";
+
+			console.log("Insert: ", st);
+
+			await connection.execute(
+				st,
+				[],
+				{ autoCommit: true },
+				function (err, result) {
+					if (err) {
+						console.log("Result: ", err);
+						res.sendStatus(400);
+						connection.close();
+					} else {
+						console.log("Result: ", result);
+						res.redirect("http://127.0.0.1:8080/");
+						connection.close();
+					}
+				}
+			);
 		});
-		
-	} catch(err){
+	} catch (err) {
 		console.error(err);
 	} finally {
 		if (connection) {
@@ -167,26 +247,65 @@ app.post("/register", async function (req, res) {
 	}
 });
 
-app.post("/upload", (req, res) => {
-	console.log(req.files);
-	console.log(req.body);
+app.post("/upload", async (req, res) => {
+	if (!loggedIn) return res.status(400).end();
 
-	const newPath = path.join(__dirname, "public", "images", uuidv4() + ".png");
-	req.files.post_picture.mv(newPath, (err) => console.log(err));
-	res.redirect("/");
+	const newName = uuidv4() + ".png";
+	const newPath = join(__dirname, "public", "images", newName);
+	if (req.files) {
+		req.files.post_picture.mv(newPath, (err) => console.log(err));
+	}
+
+	const post = {
+		username: sess.username,
+		title: req.body.post_title,
+		content: req.body.post_content,
+		tag: req.body.post_tag,
+		image_name: null,
+	};
+
+	if (req.files) {
+		post.image_name = newName;
+	}
+
+	try {
+		connection = await oracledb.getConnection({
+			user: config.database.user,
+			password: config.database.password,
+			connectString: config.database.connectString,
+		});
+
+		await connection.execute(
+			`begin
+			 addPost(:username, :title, :content, :tag, :image_name);
+			 end;`,
+			post,
+			{ autoCommit: true },
+			(err, result) => {
+				if (err) {
+					console.log("Result: ", err);
+					connection.close();
+					return res.sendStatus(400);
+				} else {
+					console.log("Result: ", result);
+					connection.close();
+					return res.redirect("/");
+				}
+			}
+		);
+	} catch (err) {
+		console.error(err);
+	} finally {
+		if (connection) {
+			try {
+				//await connection.close();
+			} catch (err) {
+				console.error(err);
+			}
+		}
+	}
+
+	//res.redirect("/");
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`));
-
-const menuLoggedIn = [
-	{ name: "profile", href: "/profile" },
-	{ name: "homepage", href: "/" },
-];
-
-const menuNotLoggedIn = [
-	{ name: "homepage", href: "/" },
-	{ name: "login", href: "/login" },
-	{ name: "register", href: "/register" },
-];
-
-const menu = loggedIn ? menuLoggedIn : menuNotLoggedIn;
+app.listen(port, () => console.log(`8gag listening on port ${port}`));
